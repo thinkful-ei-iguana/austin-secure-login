bf2484efbc77e20e893a43e9c484969774145838
AustinT33 <austintumlinson@gmail.com>
2019-12-18 14:14:06 -0600

    working on testing

--- thingful-server/src/middleware/basic-auth.js
+++ thingful-server/src/middleware/basic-auth.js
@@ -1,7 +1,33 @@
 function requireAuth(req, res, next) {
-    console.log('requireAuth')
-    console.log(req.get('Authorization'))
-    next()
+    const authToken = req.get('Authorization') || ''
+
+    let basicToken
+    if (!authToken.toLowerCase().startsWith('basic ')) {
+        return res.status(401).json({ error: 'Missing basic token' })
+    
+    } else {
+        basicToken = authToken.slice('basic '.length, authToken.length)
+    }
+
+    const [tokenUserName, tokenPassword] = Buffer
+    .from(basicTOken, 'base64')
+    .toString()
+    .split(':')
+
+    if(!tokenUserName || !tokenPassword) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+req.app.get('db')('thingful_users')
+    .where({ user_name: tokenUserName })
+    .first()
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized request' })
+        }
+        next()
+    })
+    .catch(next)
 }
 
 module.exports = {
