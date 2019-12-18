41cdf67ab921654e82573ac8003b9ed2bc8cbb9e
AnthonyTB <Anthonylol@protonmail.com>
2019-12-18 12:14:53 -0800

    pulling

--- thingful-server/src/middleware/basic-auth.js
+++ thingful-server/src/middleware/basic-auth.js
@@ -1,9 +1,12 @@
 function requireAuth(req, res, next) {
-    console.log('requireAuth')
-    console.log(req.get('Authorization'))
-    next()
+  console.log(req.get("Authorization"));
+  const { user_name, password } = req.body;
+  const user_name = req.get("Authorization");
+  const password = req.get("password");
+
+  next();
 }
 
 module.exports = {
-    requireAuth,
-}
+  requireAuth
+};
